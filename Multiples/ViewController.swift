02f2d5458f5add26ce0b90823725368be4c96205
Yogesh Kumar <yogeshkumar180592@gmail.com>
2016-02-06 02:28:09 +0530

    Added views for all the elements, created outlets as well

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var tableText: UITextField!
+    @IBOutlet weak var startBtn: UIButton!
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var equalsLabel: UILabel!
+    @IBOutlet weak var answerText: UITextField!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var againBtn: UIButton!
+    
+    
 
 }
 
